776accac8d8bdc16958ef95fe5caf1a6339366a5
Sam Daramroei <samdotmc@gmail.com>
2024-12-18 23:14:34 +0300

    adjusted some html errors and appearance in small screen

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -10,22 +10,3 @@ document.getElementById("year").textContent = currentYear;
 
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = lastModified;
-
-// Lazy loading images using Intersection Observer
-const lazyImages = document.querySelectorAll('img[loading="lazy"]');
-const imageObserver = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const image = entry.target;
-        image.src = image.dataset.src;
-        image.removeAttribute("loading");
-        observer.unobserve(image);
-      }
-    });
-  },
-  { rootMargin: "0px 0px 200px 0px" }
-);
-lazyImages.forEach((image) => {
-  imageObserver.observe(image);
-});
